aaaae563fe728ddf5bf40ff9190fefb0425ebc2a
SteelLu <jllueraochoasteel@gmail.com>
2025-09-01 15:06:18 -0500

    Se ha creado y se ha añadido el componente Productos

---
+++ src/data/contenidoProductos.js
@@ -0,0 +1,174 @@
+export const contenidoProductos = {
+    titulo: "Productos Destacados",
+    descripcion: "Descubre nuestra selección de productos de fundición más solicitados, fabricados con la más alta calidad y precisión para satisfacer las necesidades de nuestros clientes.",
+    productos: [
+        {
+            nombre: "CONO",
+            categoria: "FERROSO",
+            imagen: "imagenes/productos/conos.png",
+            descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            detalles: [
+                {
+                    titulo: "Composición Química",
+                    valor: "Al: 92%, Si: 7%, Mg: 0.3%",
+                    icono: "imagenes/productos/composicion-quimica.png"
+                },
+                {
+                    titulo: "Aplicaciones",
+                    valor: "Componentes estructurales o carcasas",
+                    icono: "imagenes/productos/aplicaciones.png"
+                },
+                {
+                    titulo: "Peso",
+                    valor: "85 - 120 Kg",
+                    icono: "imagenes/productos/peso.png"
+                },
+                {
+                    titulo: "Dureza",
+                    valor: "90 - 105 HB",
+                    icono: "imagenes/productos/dureza.png"
+                }
+            ]
+        },
+        {
+            nombre: "PIEZA INDUSTRIAL",
+            categoria: "NO FERROSO",
+            imagen: "imagenes/productos/forros-molino.png",
+            descripcion: "Componente de alta resistencia diseñado para aplicaciones industriales exigentes, fabricado con aleaciones especiales.",
+            detalles: [
+                {
+                    titulo: "Composición Química",
+                    valor: "Cu: 85%, Zn: 12%, Al: 3%",
+                    icono: "imagenes/productos/composicion-quimica.png"
+                },
+                {
+                    titulo: "Aplicaciones",
+                    valor: "Maquinaria pesada y equipos",
+                    icono: "imagenes/productos/aplicaciones.png"
+                },
+                {
+                    titulo: "Peso",
+                    valor: "45 - 75 Kg",
+                    icono: "imagenes/productos/peso.png"
+                },
+                {
+                    titulo: "Dureza",
+                    valor: "75 - 85 HB",
+                    icono: "imagenes/productos/dureza.png"
+                }
+            ]
+        },
+        {
+            nombre: "ENGRANAJE",
+            categoria: "ALEACIÓN ESPECIAL",
+            imagen: "imagenes/productos/barras-impacto.png",
+            descripcion: "Engranaje de precisión fabricado con aleaciones de alta resistencia para aplicaciones de transmisión de potencia.",
+            detalles: [
+                {
+                    titulo: "Composición Química",
+                    valor: "Fe: 95%, C: 3%, Cr: 2%",
+                    icono: "imagenes/productos/composicion-quimica.png"
+                },
+                {
+                    titulo: "Aplicaciones",
+                    valor: "Sistemas de transmisión",
+                    icono: "imagenes/productos/aplicaciones.png"
+                },
+                {
+                    titulo: "Peso",
+                    valor: "15 - 35 Kg",
+                    icono: "imagenes/productos/peso.png"
+                },
+                {
+                    titulo: "Dureza",
+                    valor: "110 - 130 HB",
+                    icono: "imagenes/productos/dureza.png"
+                }
+            ]
+        },
+        {
+            nombre: "CONO",
+            categoria: "FERROSO",
+            imagen: "imagenes/productos/conos.png",
+            descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            detalles: [
+                {
+                    titulo: "Composición Química",
+                    valor: "Al: 92%, Si: 7%, Mg: 0.3%",
+                    icono: "imagenes/productos/composicion-quimica.png"
+                },
+                {
+                    titulo: "Aplicaciones",
+                    valor: "Componentes estructurales o carcasas",
+                    icono: "imagenes/productos/aplicaciones.png"
+                },
+                {
+                    titulo: "Peso",
+                    valor: "85 - 120 Kg",
+                    icono: "imagenes/productos/peso.png"
+                },
+                {
+                    titulo: "Dureza",
+                    valor: "90 - 105 HB",
+                    icono: "imagenes/productos/dureza.png"
+                }
+            ]
+        },
+        {
+            nombre: "CONO",
+            categoria: "FERROSO",
+            imagen: "imagenes/productos/conos.png",
+            descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            detalles: [
+                {
+                    titulo: "Composición Química",
+                    valor: "Al: 92%, Si: 7%, Mg: 0.3%",
+                    icono: "imagenes/productos/composicion-quimica.png"
+                },
+                {
+                    titulo: "Aplicaciones",
+                    valor: "Componentes estructurales o carcasas",
+                    icono: "imagenes/productos/aplicaciones.png"
+                },
+                {
+                    titulo: "Peso",
+                    valor: "85 - 120 Kg",
+                    icono: "imagenes/productos/peso.png"
+                },
+                {
+                    titulo: "Dureza",
+                    valor: "90 - 105 HB",
+                    icono: "imagenes/productos/dureza.png"
+                }
+            ]
+        },
+        {
+            nombre: "CONO",
+            categoria: "FERROSO",
+            imagen: "imagenes/productos/conos.png",
+            descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            detalles: [
+                {
+                    titulo: "Composición Química",
+                    valor: "Al: 92%, Si: 7%, Mg: 0.3%",
+                    icono: "imagenes/productos/composicion-quimica.png"
+                },
+                {
+                    titulo: "Aplicaciones",
+                    valor: "Componentes estructurales o carcasas",
+                    icono: "imagenes/productos/aplicaciones.png"
+                },
+                {
+                    titulo: "Peso",
+                    valor: "85 - 120 Kg",
+                    icono: "imagenes/productos/peso.png"
+                },
+                {
+                    titulo: "Dureza",
+                    valor: "90 - 105 HB",
+                    icono: "imagenes/productos/dureza.png"
+                }
+            ]
+        },
+    ]
+};
