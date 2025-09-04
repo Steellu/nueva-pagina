e4bdd751b8d8a99370777fb0d2e0280890531fb6
SteelLu <jllueraochoasteel@gmail.com>
2025-09-04 12:55:45 -0500

    Se ha creado y agregado el componente Procesos

---
+++ src/data/contenidoProcesos.js
@@ -0,0 +1,58 @@
+export const contenidoProcesos = [
+  {
+    id: 1,
+    nombre: "Ingenieria y Diseño",
+    descripcion: "Diseñamos y desarrollamos el sistema para cada pieza, partiendo de una muestra física o de planos",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 2,
+    nombre: "Modeleria",
+    descripcion: "Contamos con un área de modeleria completa para fabricar diferentes diseños de modelos.",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 3,
+    nombre: "Moldeo",
+    descripcion: "Se utilizan diversas técnicas de moldeo, aplicando diversos insumos como filtros, manguitos exotérmicos, etc.",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 4,
+    nombre: "Fusion y Vaceado",
+    descripcion: "En esta area se realiza el proceso de vaciado del material hacia el molde.",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 5,
+    nombre: "Tratamiento Termico",
+    descripcion: "Se realizan Tratamiento de Revenido, Temple Tratamiento de Recocido, Normalizado.",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 6,
+    nombre: "Laboratorio",
+    descripcion: "En esta area realizamos mediciones de dureza, analisis de composicion quimica ademas de analisis y metalograficos de aleaciones",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 7,
+    nombre: "Acabados",
+    descripcion: "Las piezas ingresadas al area de acabados son cortados y ensamblados adecuadamente, dependiendo de la fragilidad del tipo de metal.",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  },
+  {
+    id: 8,
+    nombre: "Control de Calidad",
+    descripcion: "En esta area se mide la dureza de las piezas, para asegurarnos de que no tangan fisuras.",
+    video: "/videos/portada/videoportada.mp4",
+    portada: "/imagenes/servicios/imagen-dos.webp"
+  }
+];
