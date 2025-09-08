6f0cc331ea83a4835208a85cecee075f3c98a68e
SteelLu <jllueraochoasteel@gmail.com>
2025-09-08 15:26:26 -0500

    Se ha corregido la posicion de las flechas del componente Productos, se ha corregido el responsive del componenete Procesos y tambien se ha cambiado el video del componente Portada

--- src/data/contenidoPortada.js
+++ src/data/contenidoPortada.js
@@ -6,9 +6,9 @@ export const contenidoPortada = {
     alto: 'auto'
   },
   video: {
-    src: '/videos/portada/videoportada.mp4',
+    src: '/videos/portada/videoportada2.mp4',
     tipo: 'video/mp4',
-    poster: '/imagenes/portada/poster-video.jpg'
+    poster: '/imagenes/portada/imagen-uno.webp'
   },
   mensajes: {
     principal: 'Hace más de 35 años fundiendo el mejor metal del Perú',
